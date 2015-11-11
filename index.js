4c641b998ff28b5b94ee05f8c101d7b9c6866c4a
mattgrannary <me@mattgrannary.com>
2015-11-10 21:51:32 -0500

    initial commit

--- index.js
+++ index.js
@@ -2,5 +2,14 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-vegas'
+  name: 'ember-vegas',
+
+  included: function(app) {
+    app.import(app.bowerDirectory + '/vegas/dist/js/vegas.min.js');
+    app.import(app.bowerDirectory + '/vegas/dist/css/vegas.min.css');
+  },
+
+  isDevelopingAddon: function() {
+    return true;
+  }
 };
